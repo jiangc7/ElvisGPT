b23200484f0393634436addd8d266d173ae1938b
jiangc7 <118397495+jiangc7@users.noreply.github.com>
2023-08-29 11:57:58 +1200

    pulish to AWS version

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import bodyParser from 'body-parser';
 import cors from "cors";
 
 const openai = new OpenAI({
-    organization: "org-VeeIcrwbx3J9FMCL8fdn5x1A",
-    apiKey: "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", // This is also the default, can be omitted
+    organization: " ",
+    apiKey: " ", // This is also the default, can be omitted
 });
 
 const app = express();
@@ -21,7 +21,7 @@ app.post("/", async (req, res) => {
     const chatCompletion = await openai.chat.completions.create({
         model: "gpt-3.5-turbo",
         messages: [
-            {"role": "system", "content": "You are a DesignGPT helpful assistant by Elvis."},
+            {"role": "system", "content": "You are a ElvisGPT helpful assistant by Elvis."},
             {"role": "user", "content": `${message}`}
         ],
     });
